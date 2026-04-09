58d9261fc04a00cc50f7e5abf197247df399f2b4
malamjayesh <malamj001@gmail.com>
2026-04-09 13:20:58 +0530

    fix build time error

--- src/components/TokenVerifier.tsx
+++ src/components/TokenVerifier.tsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppDispatch } from '@/store/hooks';
 import { verifyToken } from '@/slices/authSlice';
 
 const TokenVerifier = () => {
   const dispatch = useAppDispatch();
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
     console.log('[TOKEN VERIFIER] Verifying token on mount');
